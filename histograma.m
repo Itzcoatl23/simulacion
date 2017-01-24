4989f5fd34178a9c6d22bc276424f60d0793969d
Itzcoatl23 <martin.fcfm@gmail.com>
2017-01-24 17:50:52 -0600

    Segundo avance 'camino 1'
    Se generan las partículas en función de los valores permitidos en 1
    dimensión.

--- histograma.m
+++ histograma.m
@@ -1,26 +1,44 @@
 
+% VARIABLES
+
 l=3; % mitad del espacio
 p=1.5; % valor del número de salto
-
-k=((2*l)/p)+1; % número de valores permitidos a través del espacio
-
-
+n=5; % filas / numero de partículas
+d=1; % columnas / dimensión
 
 
+% Número de valores permitidos a través
+% del espacio.
 
-%tamaño de coordenadas
-n1=10; %filas o numero de partículas
-n2=1; %columnas o dimensión
+k=((2*l)/p)+1;
 
-particulas=round(unifrnd(-1*l,l,n1,n2));
+
+% Se define una matriz 'particulas'
+% que reune las coordenadas de cada partícula,
+% una por cada fila.
+
+% Las partículas se generan aleatoreamente
+% en función de la cantidad de
+% valores permitidos.
+
+particulas=round(unifrnd(1,k,n,d));
 
 particulas
 
+% Se asigna el valor real en el espacio
+% de las 'l' a cada partícula generada.
 
-for i=1:10
-  particulas(i)=sqrt(particulas(i).^2);
+for i=1:n
+  particulas(i)=((particulas(i)-1)*p-l);
 endfor
 
 particulas
 
-hist(particulas)
+
+%for i=1:n
+%  particulas(i)=sqrt(particulas(i).^2);
+%endfor
+
+%particulas
+
+%hist(particulas)
