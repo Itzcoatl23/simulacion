218e88c18ab0e0607e148658954120826792b526
Itzcoatl23 <martin.fcfm@gmail.com>
2017-01-24 18:43:05 -0600

    En espera de revisión

--- histograma.m
+++ histograma.m
@@ -1,5 +1,5 @@
 
-% VARIABLES
+%  VARIABLES
 
 l=3; % mitad del espacio
 p=1.5; % valor del número de salto
@@ -7,38 +7,47 @@ n=5; % filas / numero de partículas
 d=1; % columnas / dimensión
 
 
-% Número de valores permitidos a través
-% del espacio.
+%  Número de valores permitidos a través
+%  del espacio.
 
 k=((2*l)/p)+1;
 
 
-% Se define una matriz 'particulas'
-% que reune las coordenadas de cada partícula,
-% una por cada fila.
+%  Se define una matriz 'particulas'
+%  que reune las coordenadas de cada partícula,
+%  una por cada fila.
 
-% Las partículas se generan aleatoreamente
-% en función de la cantidad de
-% valores permitidos.
+%  Las partículas se generan aleatoreamente
+%  en función de la cantidad de
+%  valores permitidos.
 
 particulas=round(unifrnd(1,k,n,d));
 
-particulas
 
-% Se asigna el valor real en el espacio
-% de las 'l' a cada partícula generada.
+%  particulas
+
+
+%  Se asigna el valor real en el espacio
+%  de las 'l' a cada partícula generada.
 
 for i=1:n
-  particulas(i)=((particulas(i)-1)*p-l);
+  for j=1:d
+    particulas(i,j)=((particulas(i,j)-1)*p-l);
+  endfor
 endfor
 
 particulas
 
+%  Se calcula la distancia al origen
+%  y se guardan los valores
+%  en 'distancias'.
 
-%for i=1:n
-%  particulas(i)=sqrt(particulas(i).^2);
-%endfor
+for i=1:n
+  distancias(i,1)=sqrt(sum((particulas(i,:).^2)));
+endfor
 
-%particulas
 
-%hist(particulas)
+%  distancias
+
+
+hist(distancias)
