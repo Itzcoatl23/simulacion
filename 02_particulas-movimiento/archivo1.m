58991ec7432c0200b0d3ad4902d0a0b1a3fe3cf1
Itzcoatl23 <martin.fcfm@gmail.com>
2017-01-31 14:58:58 -0600

    Avance en clase
    Se mueve aleatoriamente una partícula en una dimensión con pasos de uno
    en uno

---
+++ 02_particulas-movimiento/archivo1.m
@@ -0,0 +1,32 @@
+x=20;
+tiempos=5;
+
+
+
+for i=1:tiempos
+
+  paso=randi([1 2],1,1);
+
+  if (paso == 1)
+      x++;
+    elseif(paso == 2)
+      x--;
+    else
+      x=x;
+  endif
+
+  x
+
+endfor
+
+
+%{
+switch paso
+case 1
+  x=x++;
+case 2
+  x=x--;
+otherwise
+  x=x;
+end
+%}
